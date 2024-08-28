c649688c40a8864a89c1d5913e22e0379625cebe
madonna-mosaad <madonnamosaad86@gmail.com>
2024-08-28 22:41:52 +0300

    Add project files.

---
+++ MVC02/Controllers/MovieController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC02.Controllers
+{
+    public class MovieController: Controller
+    {
+        public IActionResult GetMovie(int id)
+        {
+            return Content($"Movie with id = {id}");
+        }
+        [ActionName("ToGetMethod")]
+        public IActionResult Index(int Id)
+        {
+            return RedirectToAction("GetMovie", new { id = Id });
+        }
+    }
+}
